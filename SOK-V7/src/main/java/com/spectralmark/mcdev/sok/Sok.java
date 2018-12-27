aadcc45800445bdf5730cd770d476c308b401ca5
Phantasmal Rin <phantasmalrin@outlook.com>
2018-12-27 10:25:45 -0800

    Optifine join reminders

--- SOK-V7/src/main/java/com/spectralmark/mcdev/sok/Sok.java
+++ SOK-V7/src/main/java/com/spectralmark/mcdev/sok/Sok.java
@@ -1,14 +1,17 @@
 package com.spectralmark.mcdev.sok;
 
+import com.spectralmark.mcdev.sok.events.EventsMain;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Sok extends JavaPlugin {
+public final class Sok extends JavaPlugin{
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Swords of Kings; ENABLED");
+        //enables join event "optifine reminder"
+        new EventsMain (this);
     }
 
     @Override
