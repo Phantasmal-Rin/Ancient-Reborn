2dc016a9387ecbdc95032cf49fa16aa6e97de300
Phantasmal Rin <phantasmalrin@outlook.com>
2018-12-28 13:03:53 -0800

    join optifine

--- AR/src/main/java/phantasmalrin/ancientsreborn/AncientsReborn.java
+++ AR/src/main/java/phantasmalrin/ancientsreborn/AncientsReborn.java
@@ -1,5 +1,6 @@
 package phantasmalrin.ancientsreborn;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AncientsReborn extends JavaPlugin {
@@ -7,6 +8,7 @@ public final class AncientsReborn extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Ancients Reborn Online");
 
     }
 
---
+++ AR/src/main/java/phantasmalrin/ancientsreborn/events/events.java
@@ -0,0 +1,21 @@
+package phantasmalrin.ancientsreborn.events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class events implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {Player player = event.getPlayer();
+        if(player.hasPlayedBefore() == false){
+            player.sendMessage(ChatColor.GOLD + "Oranis Requires Optifne for a full experience");
+        }
+        if(player.hasPlayedBefore() == true){
+            player.sendMessage(ChatColor.GOLD + "Please Enable Optifine");
+        }
+    }
+}
